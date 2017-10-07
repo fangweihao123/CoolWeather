ba2b9eeb013266eb7f9995c8daa60c9deea39c15
po <137012120@qq.com>
2017-10-07 22:25:26 +0800

    load the picture from bing's background pic

--- MyFirstApp/app/src/main/java/com/example/po/myfirstapp/Activity/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/po/myfirstapp/Activity/MainActivity.java
@@ -10,21 +10,9 @@ import com.example.po.myfirstapp.Fragment.MainFragment;
 import com.example.po.myfirstapp.R;
 
 public class MainActivity extends SingleFragmentActivity {
-    private ImageView bingPicPng;
     @Override
     protected Fragment createFragment() {
         return new MainFragment();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        bingPicPng = (ImageView) findViewById(R.id.bing_pic_png);
-
-    }
-
-    private void loadBingPic(){
-        String requestBingPic = "http://guolin.tech/api/bing_pic";
     }
 }
 
--- MyFirstApp/app/src/main/java/com/example/po/myfirstapp/Fragment/MainFragment.java
+++ MyFirstApp/app/src/main/java/com/example/po/myfirstapp/Fragment/MainFragment.java
@@ -1,20 +1,35 @@
 package com.example.po.myfirstapp.Fragment;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.example.po.myfirstapp.Activity.LocationListActivity;
+import com.example.po.myfirstapp.Activity.MainActivity;
 import com.example.po.myfirstapp.R;
+import com.example.po.myfirstapp.util.HttpUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Created by 13701 on 2017/10/5.
  */
 
 public class MainFragment extends Fragment {
+    private ImageView mImageView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +40,41 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.main_fragment,container,false);
+        mImageView = v.findViewById(R.id.bing_pic_png);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LocationListActivity.class);
+                startActivity(intent);
+            }
+        });
+        loadBingPic();
         return v;
+    }
+
+    private void loadBingPic(){
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.
+                        getDefaultSharedPreferences(getActivity()).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(getActivity()).load(bingPic).into(mImageView);
+                    }
+                });
+
+            }
+        });
     }
 }
